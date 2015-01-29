fb5d24fb63d1455915c69e96ae8fd279bfc37e41
Sindre Wetjen <sindre@wetjen.no>
2015-01-29 23:34:52 +0100

    Fix the Spotify plugin to use the Spotify Web API
    For more stable stuff, use the json Web API provided
    by Spotify, described here:
    
    https://developer.spotify.com/web-api/endpoint-reference/

--- Spotify/__init__.py
+++ Spotify/__init__.py
@@ -1,24 +1,15 @@
 # -*- coding: utf-8 -*-
 from urllib import urlopen
 import re
+import json
 import time
 import tinyurl
 
-# title_re = r'<h1 id="title">\s*<a id="title" href=".*?">([^<]*)</a>\s*</h1>'
-# artist_re = r'''<div\s*id="artist">\s*
-# <div\s*class="[^"]*">[^<]*</div>\s*
-# <p\s*class="meta-info">\s*<a\s*href="[^"]*">(.*?)</a>\s*
-# </p>\s*
-# </div>'''
-
-title_re = r'<h1\s+itemprop="name">([^<]+)</h1>'
-artist_re = r'<h2>\s*by\s*<a href="[^"]+">([^<]+)</a></h2>'   
-
-spotify_adr = r'\s*(http://open.spotify.com/[^/]*\S*)\s*'
-spotify_thing = r'\s*spotify:([^:]+):(\S*)\s*'
+spotify_adr = r'\s*(http://open.spotify.com/([^/]+)/(\S+))\s*'
+spotify_thing = r'\s*spotify:([^:]+):(\S+)\s*'
 
 class Plugin:
-        
+
     def __init__(self):
         self.spre = re.compile(spotify_adr)
         self.spt = re.compile(spotify_thing)
@@ -29,11 +20,11 @@ class Plugin:
             return [(0, channel, "{a} by {b}, {c} ".format(a = result[1], b = result[0], c = tinyurl.create_one(self.spe.grooveshark_search(result[1], result[0]))))]
         else:
             return [(0, channel, "Spotify Timed out??")]
-        
+
     def listen(self, msg, channel, **kwargs):
         match = self.spre.search(msg)
         if match:
-            return self.check_and_msg(channel, self.spe.parse_spotify(match.group(1)))
+            return self.check_and_msg(channel, self.spe.rewrite_and_parse(match.group(2), match.group(3)))
 
         match = self.spt.search(msg)
         if match:
@@ -41,36 +32,22 @@ class Plugin:
 
 class SpotifyExtract:
 
-    def __init__(self):
-        self.tre = re.compile(title_re)
-        self.are = re.compile(artist_re)
+    def to_plural(self, string):
+        "Yes, it is simpel. But will work for most cases. Update after need."
+        return string + 's'
 
-    def rewrite_and_parse(self, t, desc):
-        return self.parse_spotify("http://open.spotify.com/%s/%s" % (t, desc))
-        
+    def rewrite_and_parse(self, spotify_type, identifier):
+        return self.parse_spotify("http://api.spotify.com/v1/%s/%s" % (self.to_plural(spotify_type), identifier))
+
     def parse_spotify(self, url):
-        try:
-            artist = None
-            title = None
-            site = urlopen(url).read()
+        answer = json.loads(urlopen(url).read())
+        return (answer['artists'][0]['name'], answer['name'])
 
-            match = self.tre.search(site)
-            if match: 
-                title = match.group(1)
-
-            match = self.are.search(site)
-            if match:
-                artist = match.group(1)
-
-            return (artist, title)
-        except:
-            return None
-        
     def grooveshark_search(self, title, artist):
         title = title.replace(" ", "+")
         artist = artist.replace(" ", "+")
         return "http://grooveshark.com/#!/search?q=%s+%s" % (title, artist)
-    
+
     def youtube_search(self, title, artist):
         title = title.replace(" ", "+")
         artist = artist.replace(" ", "+")
