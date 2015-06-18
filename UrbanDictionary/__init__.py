96bbd36b4a4e3d5bf483eb360a1b9f6737c0e77f
Sindre Wetjen <sin@voyager.tjenen.de>
2015-06-18 20:12:35 +0200

    Add UrbanDictionary define plugin to define words

---
+++ UrbanDictionary/__init__.py
@@ -0,0 +1,35 @@
+import json
+from urllib import urlopen
+from cgi import escape
+
+class Plugin(object):
+
+    def __init__(self, **kwargs): pass
+
+    def cmd(self, command, args, channel, **kwargs):
+        if command == 'define' and args:
+            search_query = escape(args)
+            word =  self.search_urbandictionary(search_query)
+            if word and 'definition' in word and 'word' in word:
+                message = "{w}: {d}".format(w = word['word'].encode('utf-8'),
+                                            d = word['definition'].replace("\n", "").replace("\r", "").encode('utf-8'))
+                if len(message) > 250:
+                    r = []
+                    if 'permalink' in word:
+                        r.append((0, channel,
+                                  "For more info: {u}".format(u = word['permalink'])))
+                    r.append((0, channel, message[0:247] + "..."))
+                    return r
+                else:
+                    return [(0, channel, message)]
+            else:
+                return [(0, channel,
+                         "Sorry, I couldn't find \"{w}\"".format(w = args))]
+
+    def search_urbandictionary(self, query):
+        result = urlopen("http://api.urbandictionary.com/v0/define?term=" + query).read()
+        answer = json.loads(result)
+        if 'result_type' in answer and answer['result_type'] == 'exact':
+            return answer['list'][0]
+        else:
+            return False
