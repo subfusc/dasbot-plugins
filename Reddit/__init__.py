e2a86f6b3bfecb8c632acae41e111a8404959221
Sindre Wetjen <sindre@tjenen.de>
2016-02-06 01:13:49 +0100

    Add timeout object for aww list so we dont spam redit

--- Reddit/__init__.py
+++ Reddit/__init__.py
@@ -1,25 +1,33 @@
 # -*- coding: utf-8 -*-
 from urllib import urlopen
 from random import randint
+from time   import time
 import json
 
 class Plugin:
 
-    def __init__(self): pass
+    def __init__(self):
+        self.aww_list = None
+        self.aww_updated_at = None
 
     def cmd(self, command, args, channel, **kwargs):
         if command == 'aww' or command == 'sad' or command == 'depressed':
-            url = 'https://reddit.com/r/aww/hot.json?limit=100'
-            aww = json.loads(urlopen(url).read())
+            if self.aww_updated_at == None or (time() - self.aww_updated_at) > 600:
+                url = 'https://reddit.com/r/aww/hot.json?limit=100'
+                self.aww_list = json.loads(urlopen(url).read())
 
-            if 'error' in aww:
-                print(aww['error'])
-                return [(0, channel, kwargs['from_nick'],
-                         'I\'m so sorry, Reddit gave me an error. :(')]
+                if 'error' in self.aww_list:
+                    print(self.aww_list['error'])
+                    return [(0, channel, kwargs['from_nick'],
+                             'I\'m so sorry, Reddit gave me an error. :(')]
+                else:
+                    self.aww_updated_at = time()
 
-            item = aww['data']['children'][randint(1,len(aww['data']['children']) - 1)]
+            item = self.aww_list['data']['children'][randint(1,len(self.aww_list['data']['children']) - 1)]
             return [(0, channel, kwargs['from_nick'], item['data']['url'])]
 
 if __name__ == '__main__':
     p = Plugin()
+    print(p.cmd('aww', None, '#dasbot', from_nick='foo'))
+    print(p.cmd('aww', None, '#dasbot', from_nick='foo'))
     print(p.cmd('aww', None, '#dasbot', from_nick='foo'))
