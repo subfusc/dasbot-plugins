ac5d26c45244ebe4f54c3f28b336c0ef4a1ebb2d
Emanuele Lapponi <emanuel@1x-193-157-201-196.uio.no>
2016-02-23 11:14:54 +0100

    Insult plugin v0

---
+++ Insult/__init__.py
@@ -0,0 +1,23 @@
+import json
+from urllib import urlopen
+from xml.sax.saxutils import unescape
+
+class Plugin(object):
+
+    def __init__(self, **kwargs): pass
+
+    def cmd(self, command, args, channel, **kwargs):
+        if command == 'insult' and args:
+            insultee = args
+            return [(0, channel,
+                         "{w}: {insult}".format(w = insultee, 
+                                                insult = self.get_insult()))]
+            
+    def get_insult(self):
+        try:
+            result = urlopen("http://www.insultgenerator.org/").read()
+            result = result.split("<br><br>")[1].split("</div>")[0].strip()
+            print "RESULT: ", result
+        except:
+            result = "I could not find an appropriate insult for you. Which says it all, really."
+        return result
