99743b69a1f7a43e81475f9c212a04be0c3ec173
Emanuele Lapponi <emanlapponi@gmail.com>
2016-02-24 14:42:08 +0100

    fixed with an ugly-ass string

--- Insult/__init__.py
+++ Insult/__init__.py
@@ -9,7 +9,7 @@ class Plugin(object):
 
     def read_shake(self):
         shake_lists = [[], [], []]
-        for line in open("data/shakespeare_insult.txt"):
+        for line in self.shakestring().split('\n'):
             a, b, c = line.split()
             shake_lists[0].append(a)
             shake_lists[1].append(b)
@@ -19,7 +19,7 @@ class Plugin(object):
     def cmd(self, command, args, channel, **kwargs):
         if command == 'insult' and args:
             insultee = args
-            return [(0, channel, self.get_insult(insultee))]
+            return [(0, channel, self.get_insult(insultee.strip()))]
 
     def get_insult(self, insultee):
         try:
@@ -42,7 +42,59 @@ class Plugin(object):
 
     def from_shake(self, insultee):
         length = len(self.shake_insults[0]) - 1
-        return "%s, you %s %s %s" % (insultee,
+        return "%s, you %s %s %s." % (insultee,
                                      self.shake_insults[0][randint(0, length)],
                                      self.shake_insults[1][randint(0, length)],
                                      self.shake_insults[2][randint(0, length)])
+    def shakestring(self):
+        return """artless             base-court          apple-john
+bawdy               bat-fowling         baggage
+beslubbering        beef-witted         barnacle
+bootless            beetle-headed       bladder
+churlish            boil-brained        boar-pig
+cockered            clapper-clawed      bugbear
+clouted             clay-brained        bum-bailey
+craven              common-kissing      canker-blossom
+currish             crook-pated         clack-dish
+dankish             dismal-dreaming     clotpole
+dissembling         dizzy-eyed          coxcomb
+droning             doghearted          codpiece
+errant              dread-bolted        death-token
+fawning             earth-vexing        dewberry
+fobbing             elf-skinned         flap-dragon
+froward             fat-kidneyed        flax-wench
+frothy              fen-sucked          flirt-gill
+gleeking            flap-mouthed        foot-licker
+goatish             fly-bitten          fustilarian
+gorbellied          folly-fallen        giglet
+impertinent         fool-born           gudgeon
+infectious          full-gorged         haggard
+jarring             guts-griping        harpy
+loggerheaded        half-faced          hedge-pig
+lumpish             hasty-witted        horn-beast
+mammering           hedge-born          hugger-mugger
+mangled             hell-hated          joithead
+mewling             idle-headed         lewdster
+paunchy             ill-breeding        lout
+pribbling           ill-nurtured        maggot-pie
+puking              knotty-pated        malt-worm
+puny                milk-livered        mammet
+qualling            motley-minded       measle
+rank                onion-eyed          minnow
+reeky               plume-plucked       miscreant
+roguish             pottle-deep         moldwarp
+ruttish             pox-marked          mumble-news
+saucy               reeling-ripe        nut-hook
+spleeny             rough-hewn          pigeon-egg
+spongy              rude-growing        pignut
+surly               rump-fed            puttock
+tottering           shard-borne         pumpion
+unmuzzled           sheep-biting        ratsbane
+vain                spur-galled         scut
+venomed             swag-bellied        skainsmate
+villainous          tardy-gaited        strumpet
+warped              tickle-brained      varlot
+wayward             toad-spotted        vassal
+weedy               unchin-snouted      whey-face
+yeasty              weather-bitten      wagtail"""
+
