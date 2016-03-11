2ef7b16d726cfb65a9b64fd5e9c4ef4c2634b594
Trond Thorbjørnsen <trondth@ifi.uio.no>
2016-03-11 09:28:23 +0100

    Rephrase: help, -en, prettier print

--- Rephrase/__init__.py
+++ Rephrase/__init__.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import sys
 import string
 import random
@@ -9,6 +10,9 @@ import re
 from itertools import chain
 from urllib import urlopen
 
+re_punct = re.compile('([{}])'.format(re.escape(punctuation)), flags=re.UNICODE)
+re_punctspace = re.compile(u'([!?.,:;%)])', flags=re.UNICODE)
+
 class Plugin(object):
     def __init__(self, **kwargs):
         self.lastsay = {}
@@ -17,6 +21,14 @@ class Plugin(object):
         self.slen = 3
         self.case = 'sent'
         pass
+
+    def help(self, command, argc, channel, **kwargs):
+        if command == 'all':
+            return [(1, kwargs['from_nick'], 'Rephrase: rephrase')]
+        if command == 'rephrase':
+            return [(1, kwargs['from_nick'], conf.COMMAND_CHAR + "rephrase [-en] [nick]"),
+             (1, kwargs['from_nick'], conf.COMMAND_CHAR + "rephrase nick en"),
+             (1, kwargs['from_nick'], "Substitutes some of the words from last message (of length > {}) (from nick). Default norwegian, en for english.".format(self.slen))]
 
     def listen(self, msg, channel, **kwargs):
         if len(msg.split()) > self.slen:
@@ -67,6 +79,7 @@ class Plugin(object):
     
     def pretty_print(self, sent):
         pp = "".join([w.strip() for w in re.split('([%s])' % (punctuation), sent)])
+        pp = re_punctspace.sub("\g<1> ", pp)
         pp = pp[0].upper() + pp[1:]
         return pp
 
@@ -75,6 +88,14 @@ class Plugin(object):
             lang = 'norge'
             if args:
                 args = args.split()
+                if args[0] == '-en':
+                    lang = 'enwiki'
+                    if len(args) == 2:
+                        args[0] = args[1]
+                    else:
+                        rephrased = self.rephrase(self.verylastsay[channel], lang)
+                        rephrased = self.pretty_print(rephrased)
+                        return [(0, channel, rephrased)]
                 if len(args) == 2:
                     lang = 'enwiki' # any 2. arg -> en
                 args = args[0]
@@ -95,8 +116,15 @@ if __name__ == '__main__':
     print('done')
     p = Plugin()
     p.listen('Slip them into different sleeves! Buy both, and be deceived', '#bar', from_nick = 'baz')
-    print p.cmd('rephrase', 'baz en', '#bar')
-    p.listen('Ingenting er morsommere enn hammermessige irc-botter.', '#bar', from_nick = 'baz')
     print p.lastsay
+    print p.cmd('rephrase', 'baz en', '#bar')
+    print p.cmd('rephrase', '-en', '#bar')
+    print p.cmd('rephrase', '-en baz', '#bar')
+    p.listen('Ingenting er morsommere enn hammermessige irc-botter.', '#bar', from_nick = 'baz')
     print p.cmd('rephrase', 'baz', '#bar')
-    print p.cmd('rephrase', 'baz', '#bar')
+    p.listen(u'Ingenting. er morsommere, ø, enn hammermessige! irc-botter.', '#bar', from_nick = 'baz')
+    svar = p.cmd('rephrase', 'baz', '#bar')
+    print svar[0][2]
+    
+
+    
