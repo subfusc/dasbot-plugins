31e316f3bac29678ed964a1bb2331509a102c769
Sindre Wetjen <sindre@tjenen.de>
2016-04-06 21:02:05 +0200

    The help_char only functionality was removed

--- Useless/__init__.py
+++ Useless/__init__.py
@@ -49,8 +49,7 @@ class Plugin(object):
         if command == 'losers':
             return [(0, channel, kwargs['from_nick'], 'The losers in this channel are: ' + ", ".join([ u for u in kwargs['channel_users'] if not (u == NICK or u in kwargs['nick_to_user'])]))]
         if command == 'help':
-            return [(1, kwargs['from_nick'], "To get help for a given command use ? instead of !."),
-                    (1, kwargs['from_nick'], "To get help about help, use ? only.")]
+            return [(1, kwargs['from_nick'], "To get help for a given command use ? instead of !.")]
         if command == 'say' and args != None:
             alist = args.split()
             return [(0, alist[0], " ".join(alist[1:]))]
